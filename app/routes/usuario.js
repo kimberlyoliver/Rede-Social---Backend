693ae229ba935a3cc3170c06bd798be5240f3159
kimberlyoliver <kikyoliveira@gmail.com>
2018-06-18 20:03:53 -0300

    usuarios

--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -1,9 +1,12 @@
 let usuario = require('../controllers/usuario')
+let auth = require('../controllers/auth')
 
 module.exports = function (app) {
+    app.post('/api/usuarios/signin', auth.signin)
+    app.post('/api/usuarios', usuario.inserirUsuario)
+    app.use('/api/uruarios', auth.verifyToken)
     app.get('/api/usuarios', usuario.getUsuarios)
     app.get('api/usuarios/:id', usuario.getUsuarioById)
-    app.post('/api/usuarios', usuario.inserirUsuario)
     app.put('/api/usuarios/:id', usuario.updateUsuarioById)
     app.delete('/api/usuarios/:id', usuario.removeUsuarioById)  
 }
