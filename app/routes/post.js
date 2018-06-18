05ab029bda4e724b73cdfa288a751d2321eaf304
kimberlyoliver <kikyoliveira@gmail.com>
2018-06-18 20:05:48 -0300

    posts

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,6 +1,8 @@
 let post = require('../controllers/post')
+let auth = require('../controllers/auth')
 
 module.exports = function (app) {
+    app.use('/api/posts', auth.verifyToken)
     app.get('/api/posts', post.getPosts)
     app.get('api/posts/:id', post.getPostById)
     app.post('/api/posts', post.inserirPost)
