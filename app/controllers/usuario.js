508d915c970d51f9fd3aeaf58de0daca89391686
kimberlyoliver <kikyoliveira@gmail.com>
2018-06-18 21:08:45 -0300

    trabalho 2

--- app/controllers/usuario.js
+++ app/controllers/usuario.js
@@ -2,7 +2,7 @@ let Usuario = require('../models/usuario.js')
 let Post = require('../models/post.js')
 let bcrypt = require('bcrypt')
 
-module.exports.inserirUsuario = function (rec,res) {
+module.exports.inserirUsuario = function (req,res) {
     let usuario = new Usuario({
         nome: req.body.nome,
         email: req.body.email,
@@ -26,7 +26,7 @@ module.exports.inserirUsuario = function (rec,res) {
     )
 }
 
-module.exports.getUsuarios = function(rec, res){
+module.exports.getUsuarios = function(req, res){
     let promise = Usuario.find()
     promise.then(
         function(usuarios){
